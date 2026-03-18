0e7bcb835a1ad383b69c63fc0d70488abeeff5fb
MarianaChiorboli <marii.costa260531@gmail.com>
2026-03-18 20:15:45 -0300

    fix: trocar localhost pelo IP da EC2

--- ServicosEmNuvem-V3/front_end/script.js
+++ ServicosEmNuvem-V3/front_end/script.js
@@ -1,6 +1,6 @@
 document.getElementById('botaoFoto').addEventListener('click', () => {
     const imgElement = document.getElementById('foto');
 
-    imgElement.src = 'http://localhost:25000/foto';
+    imgElement.src = 'http://54.80.144.100:8080/foto';
     imgElement.style.display = 'block';
 });
