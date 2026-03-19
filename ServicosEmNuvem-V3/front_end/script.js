9475cfe84c68cf2f6f64fc313066c63bddb8a6a3
MarianaChiorboli <marii.costa260531@gmail.com>
2026-03-18 22:27:19 -0300

    correção url para privada

--- ServicosEmNuvem-V3/front_end/script.js
+++ ServicosEmNuvem-V3/front_end/script.js
@@ -1,6 +1,6 @@
 document.getElementById('botaoFoto').addEventListener('click', () => {
     const imgElement = document.getElementById('foto');
 
-    imgElement.src = 'http://54.80.144.100:25000/foto';
+    imgElement.src = 'http://172.31.30.31:25000/foto';
     imgElement.style.display = 'block';
 });
